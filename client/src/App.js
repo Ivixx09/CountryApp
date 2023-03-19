47c9e84998cb27272e9452c54e53c50760849517
Ivixx09 <estudio.xx23@gmailcom>
2023-03-19 17:51:34 -0300

    Prueba 6

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+// import LandingPage from './components/LandingPage';
+// import Home from './components/Home';
+// import ActivityCreate from './components/CreateActivity'
+// import Detail from './components/Detail';
+import axios from "axios";
+axios.defaults.baseURL= "http://localhost:3001/"
 
 function App() {
   return (
+  <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Switch>
+          {/* <Route exact path='/' component={LandingPage}/>
+          <Route exact path='/home' component={Home}/>
+          <Route exact path='/activity' component={ActivityCreate}/>
+          <Route exact path='/home/:id' component={Detail}/> */}
+      </Switch>
     </div>
+  </BrowserRouter>
   );
 }
 
