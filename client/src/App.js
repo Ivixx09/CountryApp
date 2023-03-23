96110cb4ee36d244ea8435b4c6de3f0a10e9509e
Ivixx09 <estudio.xx23@gmail.com>
2023-03-23 15:00:58 -0300

    New backend

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home';
 import ActivityCreate from './components/CreateActivity'
 import Detail from './components/Detail';
 import axios from "axios";
-axios.defaults.baseURL= "http://localhost:3001/"
+axios.defaults.baseURL= "https://countryapp-production-d29c.up.railway.app/"
 
 function App() {
   return (
